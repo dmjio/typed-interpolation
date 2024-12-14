d6489c31aefd15203b6eee7741dd617f773e074b
dmjio <code@dmj.io>
2024-12-13 18:23:38 -0600

    update copyright, maintainer

--- src/Interpolation.hs
+++ src/Interpolation.hs
@@ -11,9 +11,9 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Interpolation
--- Copyright   :  (C) 2023 David M. Johnson
+-- Copyright   :  (C) 2025 David M. Johnson
 -- License     :  BSD3-style (see the file LICENSE)
--- Maintainer  :  David M. Johnson <djohnson.m@gmail.com>
+-- Maintainer  :  David M. Johnson <code@dmj.io>
 -- Stability   :  experimental
 -- Portability :  non-portable
 -----------------------------------------------------------------------------
